9220f64a52bf7d14bae5d92d550ca21275f239c6
Диана Хрустовская <hrusty96@mail.ru>
2019-04-20 23:49:39 +0300

    Основная логика

--- ThreatDatabase/MainWindow.xaml.cs
+++ ThreatDatabase/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,149 @@ using System.Windows.Shapes;
 
 namespace ThreatDatabase
 {
+
+    public delegate void ButtonAction();
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ObservableCollection<Threat> listOfThreats = new ObservableCollection<Threat>();
+        private int pageNumber;
+        private int PageNumber
+        {
+            get
+            {
+                return pageNumber;
+            }
+            set
+            {
+                pageNumber = value;
+                PageLabel.Text = (pageNumber + 1).ToString();
+            }
+        }
+        private int pageCapacity = 25;
+        private bool dataBaseNotFound;
+
+        Database db = new Database();
+
+        ButtonAction messageButtonAction;
+
         public MainWindow()
         {
             InitializeComponent();
+            ListOfThreats.ItemsSource = listOfThreats;
+
+            if (db.Database.Exists())
+            {
+                SetPage(0);
+                return;
+            }
+            else
+            {
+                dataBaseNotFound = true;
+                ShowMessage("Привет", "В данный момент база данных угроз не загружена. Хотите загрузить с официального сайта?", UpdateDatabase, "Загрузить");
+            }
+
+        }
+
+
+        public void SetPage(int i)
+        {
+            if (i < 0 || i > db.Threats.Count() / pageCapacity)
+                return;
+
+            PageNumber = i;
+
+            listOfThreats.Clear();
+            int startIndex = pageCapacity * PageNumber;
+            var currentPage = db.Threats.OrderBy(x => x.Number).Skip(startIndex).Take(pageCapacity);
+            foreach (var item in currentPage)
+            {
+                listOfThreats.Add(item);
+            }
+        }
+
+
+        private void ShowMessage(string title, string text, ButtonAction action = null, string buttonText = "Ок")
+        {
+            MessageLayerTitle.Content = title;
+            MessageLayerText.Text = text;
+            MessageLayerButton.Content = buttonText;
+
+            if (action == null)
+            {
+                MessageLayerButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                messageButtonAction = action;
+                MessageLayerButton.Visibility = Visibility.Visible;
+            }
+
+            MessageLayer.Visibility = Visibility.Visible;
+        }
+        private void HideMessage()
+        {
+            MessageLayer.Visibility = Visibility.Collapsed;
+        }
+
+        public async void UpdateDatabase()
+        {
+            ShowMessage("Идёт загрузка...", "");
+
+            Queue<ThreatChange> changes;
+            try
+            {
+                changes = await Task.Run(db.Update);
+            }
+            catch (Exception e)
+            {
+                ShowMessage("Произошла ошибка", e.Message, UpdateDatabase, "Повторить");
+                return;
+            }
+
+            if (changes.Count < 1)
+            {
+                ShowMessage("Готово", "Обновлений не найдено", HideMessage);
+                return;
+            }
+
+            HideMessage();
+
+            if (!dataBaseNotFound)
+            {
+                new ThreatWindow(changes).Show();
+            }
+
+        }
+
+        private void MessageLayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            messageButtonAction?.Invoke();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDatabase();
+        }
+
+        private void LeftPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetPage(PageNumber - 1);
+        }
+
+        private void RightPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetPage(PageNumber + 1);
+        }
+
+        private void ListOfThreats_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (!(ListOfThreats.SelectedItem is Threat item)) return;
+
+            new ThreatWindow(item).Show();
         }
     }
 }
---
+++ ThreatDatabase/Threat.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThreatDatabase
+{
+    public class Threat : INotifyPropertyChanged
+    {
+        [Key]
+        public int ID { get; set; }
+
+        private int _number;
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+
+            set
+            {
+                _number = value;
+                OnNotifyPropertyChanged("Number");
+                OnNotifyPropertyChanged("FullNumber");
+            }
+
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+                OnNotifyPropertyChanged("Name");
+            }
+
+        }
+
+        public string Discription { get; set; }
+        public string Source { get; set; }
+        public string Object { get; set; }
+        public bool IsPrivacyViolation { get; set; }
+        public bool IsIntegrityViolation { get; set; }
+        public bool IsAccessibilityViolation { get; set; }
+        public long LastUpdateTime { get; set; }
+
+        [NotMapped]
+        public string FullNumber => "УБИ." + _number.ToString().PadLeft(3, '0');
+
+        public Threat Clone()
+        {
+            return new Threat()
+            {
+                ID = ID,
+                Number = Number,
+                Name = Name,
+                Discription = Discription,
+                Source = Source,
+                Object = Object,
+                IsPrivacyViolation = IsPrivacyViolation,
+                IsIntegrityViolation = IsIntegrityViolation,
+                IsAccessibilityViolation = IsAccessibilityViolation,
+                LastUpdateTime = LastUpdateTime
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnNotifyPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
